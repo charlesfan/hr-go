e3bd292fdc87c02a6c6e69fae023c41f1314cd33
CharlesFan <rock9030@gmail.com>
2025-02-18 22:47:40 +0800

    feature(sql): sql init

--- repository/db/db_init.go
+++ repository/db/db_init.go
@@ -1,3 +1,4 @@
 package db
 
-var InitSqlStr = ``
+// admin user, pwd = admin@init
+var InitSqlStr = `INSERT IGNORE INTO hr_employee VALUES ('60aacf8b-4ff9-4d46-bd53-f14c0fbf9a8e','admin','admin@init.com','$2a$10$C1PYnbqMWn3POHSf/cOlBOd0/kSfE89KRhisk1e5yujl9rwTPTAoG','hr',UNIX_TIMESTAMP(NOW()),NOW(),NOW(),NULL);`
